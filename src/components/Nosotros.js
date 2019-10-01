ffd99917a13d1d8f72afc2134998b6019aac0881
DiegoRosas12 <de.rosasgonzalez@ugto.mx>
2019-10-01 13:18:41 -0500

    creacion archivos noticias nosotros

---
+++ src/components/Nosotros.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Nosotros extends Component {
+    state = {  }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
---
+++ src/components/Noticia.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+
+export default class Noticia extends Component {
+    render(props) {
+        return (
+            <div className={style.evento}>
+                <img src={this.props.imagen} alt={this.props.img_descr} />
+                <div className={style.descr}>
+                    <h3>{this.props.titulo}</h3>
+                    <p>{this.props.texto}</p>
+                    <Redes facebook={this.props.facebook} instagram={this.props.instagram} twitter={this.props.twitter} />
+                </div>
+            </div>
+        );
+    }
+}
