588faab5536062b2465ab953610d97aaddfb2006
Alan Alvarez <alanalvarez98.aa@gmail.com>
2019-09-30 22:51:38 -0500

    Css modules example

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import styles from './header.module.css'
 
 export default class Header extends Component {
 
     render() {
         return (
-            <div className="header">
-                
+            <div className={styles.color}>
+                <h1>Hola</h1>
             </div>            
         );
     }
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
+import Header from './Header';
 
 export default class Router extends Component {
 
     render() {
         return (
             <BrowserRouter>
-
+                <Header />
                 <Switch>
                     {/* <Route exact path="/"component={Home} /> */}
                     {/* <Route component={Error}/> */}
