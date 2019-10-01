c6d4ca3c268bbe897878838a598d6a79706fdd02
Alan Alvarez <alanalvarez98.aa@gmail.com>
2019-09-30 23:25:51 -0500

    creación del componente header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import styles from './header.module.css'
+import {Link} from 'react-router-dom';
+import styles from './css/header.module.css';
 
 export default class Header extends Component {
 
     render() {
         return (
-            <div className={styles.color}>
-                <h1>Hola</h1>
+            <div className={styles.header}>
+                <Link to="/">
+                   <img className={styles.imagen} src="/img/BD_nombre.png" alt="BeeDevelopers;" />
+                </Link>
+                <div className={styles.links}>
+                    <Link to="/"><p>Home</p></Link>
+                    <Link to="/nosotros"><p>Nosotros</p></Link>
+                    <Link to="/eventos"><p>Eventos</p></Link>
+                    <Link to="/blog"><p>Blog</p></Link>
+                    <Link to="/contacto"><p>Contacto</p></Link>
+                </div>
             </div>            
         );
     }
