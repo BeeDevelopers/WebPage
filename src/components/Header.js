d25eb5f05fd7a5c2be8d9f695e0c26a2f9f4dda1
Alan Alvarez <alanalvarez98.aa@gmail.com>
2020-02-28 23:02:44 -0600

    Añadí React-bootstrap y modifiqué la navbar para que sea responsiva

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import styles from './css/header.module.css';
+import {Navbar, Nav} from 'react-bootstrap';
+
 
 export default class Header extends Component {
 
     render() {
         return (
-            <div className={styles.header}>
-                <Link to="/">
-                   <img className={styles.imagen} src="/img/BD_nombre.png" alt="BeeDevelopers;" />
-                </Link>
-                <div className={styles.links}>
-                    <Link to="/"><p>Home</p></Link>
-                    <Link to="/nosotros"><p>Nosotros</p></Link>
-                    <Link to="/eventos"><p>Eventos</p></Link>
-                    <Link to="/noticias"><p>Noticias</p></Link>
-                    <Link to="/contacto"><p>Contacto</p></Link>
-                </div>
-            </div>            
+            // <div className={styles.header}>
+            //     <Link to="/">
+            //        <img className={styles.imagen} src="/img/BD_nombre.png" alt="BeeDevelopers;" />
+            //     </Link>
+            //     <div className={styles.links}>
+            //         <Link to="/"><p>Home</p></Link>
+            //         <Link to="/nosotros"><p>Nosotros</p></Link>
+            //         <Link to="/eventos"><p>Eventos</p></Link>
+            //         <Link to="/noticias"><p>Noticias</p></Link>
+            //         <Link to="/contacto"><p>Contacto</p></Link>
+            //     </div>
+            // </div>       
+
+                <Navbar collapseOnSelect expand="lg"   className={styles.backgroundbee} >
+                    <Navbar.Brand href="/">
+                        <img className={styles.imagen} src="/img/BD_nombre.png" alt="BeeDevelopers;" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link className={styles.links} href="/nosotros"><p>Nosotros</p></Nav.Link>
+                            <Nav.Link className={styles.links} href="/eventos"><p>Eventos</p></Nav.Link>
+                            <Nav.Link className={styles.links} href="/noticias"><p>Noticias</p></Nav.Link>
+                            <Nav.Link className={styles.links} href="/contactos"><p>Contactos</p></Nav.Link>
+                        </Nav>
+                        
+                    </Navbar.Collapse>
+                </Navbar>
+
+
+
         );
     }
 }
+
