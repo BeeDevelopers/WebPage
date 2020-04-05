e8f8f584a3aff959161729bae61400a7b651fbcc
Alan Alvarez <alanalvarez98.aa@gmail.com>
2020-04-04 23:19:35 -0600

    Seccion devteam

---
+++ src/components/Persona.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export default class Persona extends Component {
+    render(props) {
+        return (
+            <div className="text-center card border-0 my-2">
+                    <img src={this.props.img} alt="..." class="mx-auto img-responsive rounded-circle" width="175" />
+                    <h2 className="font-weight-bold my-2">{this.props.nombre}</h2>
+            </div>
+        );
+    }
+}
