844797fbc095a869b32b52db6c7e451f9de9a810
Diego Rosas <de.rosasgonzalez@ugto.mx>
2020-04-05 14:29:15 -0500

    El skill ya cambia de color segun el nombre que se le ponga

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -3,10 +3,29 @@ import React, { Component } from 'react';
 import styles from './css/skill.module.css';
 
 export default class Skill extends Component {
-    state = {  }
+    constructor(props){
+        super(props);
+        this.state = {color: 'blue'}
+    }
+
+    componentDidMount(){
+        // Aquí se ponen los colores de los skills
+        switch (this.props.title.toLowerCase()) {
+            case "vue":
+                this.setState({color: '#13E564'})
+                break;
+            case "firebase":
+                this.setState({color: '#E59C13'})
+                break;            
+            default:
+                this.setState({color: '#1685D9'})
+                break;
+        }
+    }
     render() {
+        let skill
         return (
-            <div className={styles.container} style={{backgroundColor: this.props.color}}>
+            <div className={styles.container} style={{backgroundColor: this.state.color}}>
                 <p>{this.props.title}</p>
             </div>
         );
